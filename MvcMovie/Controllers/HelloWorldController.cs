7aad7d0fda5062ebf787c17471dfa5a31ebd10f6
xufeng <zzhxufeng@163.com>
2023-11-02 20:51:37 +0800

    添加项目文件。

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, string numtimes)
+        {
+            var sb = new StringBuilder();
+            var total = int.Parse(numtimes);
+            for (var i = 0; i < total; i++)
+            {
+                sb.Append(name);
+            }
+            return HtmlEncoder.Default.Encode(sb.ToString());
+        }
+    }
+}
